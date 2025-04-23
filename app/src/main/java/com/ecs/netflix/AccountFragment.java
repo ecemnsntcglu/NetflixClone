e8f8d76bcc0c7bb9633838785070e03abdddeff4
0Ecemnur22 <ecemsntc@gmail.com>
2025-04-23 16:58:01 +0300

    bottom nav ekleniyor

---
+++ app/src/main/java/com/ecs/netflix/AccountFragment.java
@@ -0,0 +1,38 @@
+package com.ecs.netflix;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ecs.netflix.databinding.FragmentAccountBinding;
+import com.ecs.netflix.databinding.FragmentFeedBinding;
+
+
+public class AccountFragment extends Fragment {
+
+    private FragmentAccountBinding binding;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentAccountBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        return view;
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
