c7aace893fe36fc91c1d4d7123cbf28a662aa70b
0Ecemnur22 <ecemsntc@gmail.com>
2025-04-23 17:22:26 +0300

    act

--- app/src/main/java/com/ecs/netflix/MainActivity.java
+++ app/src/main/java/com/ecs/netflix/MainActivity.java
@@ -2,30 +2,55 @@ package com.ecs.netflix;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
+import com.ecs.netflix.databinding.ActivityMainBinding;
 import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String ActivityMainBinding binding;
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
         Log.d("FIREBASE", "Before init");
         FirebaseApp.initializeApp(this);
         Log.d("FIREBASE", "After init");
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
+
+        NavHostFragment navHostFragment = (NavHostFragment)
+                getSupportFragmentManager().findFragmentById(R.id.navHostFragment);
+        if (navHostFragment != null) {
+            NavigationUI.setupWithNavController(binding.bottomNav,
+                    navHostFragment.getNavController());
+
+            navHostFragment.getNavController().addOnDestinationChangedListener((controller, destination, arguments) -> {
+                if (destination.getId() == R.id.kullaniciFragment) {
+                    binding.bottomNav.setVisibility(View.GONE);
+                } else {
+                    binding.bottomNav.setVisibility(View.VISIBLE);
+                }
+            });
+
+        }
+        }
 }
